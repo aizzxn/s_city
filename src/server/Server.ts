5c9f3e5cd232789e666aaadb24a232513512e9b0
victorglim <victor.guedes888@gmail.com>
2023-02-07 02:37:37 -0300

    build: built git ignore path node_modules and yarn.lock

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,5 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const server = express();
+
+
+server.get('/', (req: Request, res: Response) => {
+    return res.send('oi')
+});
 
 export { server };
