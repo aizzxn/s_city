4d4a97cd792fab72e06f27f918a9594c9c944082
victorglim <victor.guedes888@gmail.com>
2023-02-07 03:09:12 -0300

    build: built eslintrc and config package json with eslint

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from "express";
 
 const server = express();
 
-
-server.get('/', (req: Request, res: Response) => {
-    return res.send('oi')
+server.get("/", (req: Request, res: Response) => {
+  return res.send("oi");
 });
 
 export { server };
